5e01be238c4e47898b5e7805523f84369d581009
RB <rb123456>
2021-07-28 15:30:16 -0700

    Version 0.1

---
+++ src/BrowserActions/Browser.java
@@ -0,0 +1,69 @@
+package BrowserActions;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+import org.testng.Assert;
+
+public class Browser {
+  @Test
+  public void f() throws Exception{
+	  System.setProperty("webdriver.chrome.driver", "D://Ritu//Testing Training//Selenium//chromedriver_win32//chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+      driver.get("http://www.dollardays.com");
+      driver.manage().window().maximize();
+      
+      String actualTitle = driver.getTitle();
+      System.out.println (actualTitle);
+      
+      String expectedTitle = "DollarDays | Bulk Goods for Nonprofits, Businesses & Schools | Wholesale Products";
+      
+      if(actualTitle.equalsIgnoreCase(expectedTitle))
+			System.out.println("Title Matched");
+		else
+			System.out.println("Title didn't match");
+      
+      driver.close();
+  }      
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
